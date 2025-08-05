fd5a63d450db26c9ab235f89d58c253f76b69544
hongmon1 <hongmon1@naver.com>
2025-08-05 22:41:51 +0900

    glad 추가2

--- Dependency.cmake
+++ Dependency.cmake
@@ -26,17 +26,5 @@ add_subdirectory(external/glfw)
 set(DEP_LIBS ${DEP_LIBS} glfw)
 
 # glad
-ExternalProject_Add(
-    dep_glad
-    GIT_REPOSITORY "https://github.com/Dav1dde/glad"
-    GIT_TAG "v0.1.34"
-    GIT_SHALLOW 1
-    UPDATE_COMMAND ""
-    PATCH_COMMAND ""
-    CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX=${DEP_INSTALL_DIR}
-        -DGLAD_INSTALL=ON
-    TEST_COMMAND ""
-    )
-set(DEP_LIST ${DEP_LIST} dep_glad)
+add_subdirectory(external/glad)
 set(DEP_LIBS ${DEP_LIBS} glad)
